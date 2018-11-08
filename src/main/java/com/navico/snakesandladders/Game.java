69b1b222bdcc4371a8578cf90a964b7acf611fb2
Javier Gómez <jagomez@aventia.com>
2018-11-08 21:41:51 +0000

    [GREEN] Add minimum code to pass the test.

--- src/main/java/com/navico/snakesandladders/Game.java
+++ src/main/java/com/navico/snakesandladders/Game.java
@@ -12,7 +12,9 @@ public class Game {
 	}
 
 	public Integer moveToken(Integer spaces) {
-		position += spaces;
+		if ((this.position + spaces) <= BOARD_SIZE) {
+			this.position += spaces;
+		}
 		return position;
 	}
 
